c286d89ff2eeae9b0714154cc245a22b19c36863
IsaacAyaC1FC0 <isaac.ayala@cifco.gob.sv>
2025-10-10 11:48:24 -0600

    Publicacion de carpeta de plantillas de correo de laravel.

--- resources/views/emails/users/password-reset.blade.php
+++ resources/views/emails/users/password-reset.blade.php
@@ -1,22 +1,24 @@
 <x-mail::message>
+{{-- Logo --}}
+<div style="text-align: center; margin-bottom: 20px;">
+    <img src="{{ $message->embed(public_path('img/LOGO CIFCO 1 TINTA-01.png')) }}" alt="Logo CIFCO" width="150">
+</div>
+
+{{-- Título --}}
+<h1 style="text-align: center;">Notificación de Reseteo de Contraseña</h1>
+
+{{-- Contenido --}}
 <div style="text-align: center;">
-    <!-- Logo centrado -->
-    <img src="{{ asset('img/LOGO CIFCO 1 TINTA-01.png') }}" alt="CIFCO" width="150" style="margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto;">
+    Hola **{{ $user->name }}**,
 
-    <!-- Título y contenido centrado -->
-    <h1 style="text-align: center;">Notificación de Reseteo de Contraseña</h1>
+    Tu contraseña ha sido reseteada por un administrador.
 
-    <p style="text-align: center;">
-        Hola <strong>{{ $user->name }}</strong>,<br><br>
+    Tu nueva contraseña es: **{{ $newPassword }}**
 
-        Tu contraseña ha sido reseteada por un administrador.<br><br>
+    Te recomendamos encarecidamente que inicies sesión y cambies esta contraseña por una de tu elección lo antes posible desde tu perfil.
 
-        Tu nueva contraseña es: <strong>{{ $newPassword }}</strong><br><br>
-
-        Te recomendamos encarecidamente que inicies sesión y cambies esta contraseña por una de tu elección lo antes posible desde tu perfil.<br><br>
-
-        Gracias,<br>
-        Unidad de Informática
-    </p>
+    Gracias,<br>
+    Unidad de Informática
 </div>
 </x-mail::message>
+
