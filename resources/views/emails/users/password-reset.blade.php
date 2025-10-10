2fa09c4b8b109e0678089e6f15fe0808702703b9
IsaacAyaC1FC0 <isaac.ayala@cifco.gob.sv>
2025-10-10 12:13:25 -0600

    Cambios en el formato de correos.

--- resources/views/emails/users/password-reset.blade.php
+++ resources/views/emails/users/password-reset.blade.php
@@ -8,17 +8,8 @@
 <h1 style="text-align: center;">Notificación de Reseteo de Contraseña</h1>
 
 {{-- Contenido --}}
-<div style="text-align: center;">
-    Hola **{{ $user->name }}**,
-
-    Tu contraseña ha sido reseteada por un administrador.
-
-    Tu nueva contraseña es: **{{ $newPassword }}**
-
-    Te recomendamos encarecidamente que inicies sesión y cambies esta contraseña por una de tu elección lo antes posible desde tu perfil.
-
-    Gracias,<br>
-    Unidad de Informática
+<div style="text-align: center; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';">
+    Hola <strong>{{ $user->name }}</strong>,<br><br>Tu contraseña ha sido reseteada por un administrador.<br><br>Tu nueva contraseña es: <strong>{{ $newPassword }}</strong><br><br>Te recomendamos encarecidamente que inicies sesión y cambies esta contraseña por una de tu elección lo antes posible desde tu perfil.<br><br>Gracias,<br>Unidad de Informática
 </div>
-</x-mail::message>
 
+</x-mail::message>
